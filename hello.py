c78b6bf0d3eedc3195f990d2f7a966149b58f706
Hwang Joonhyung <hwang@base.envia.pe.kr>
2016-12-06 13:16:44 +0900

    revised heading

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8; -*- # PEP 263
-# vim: set fileencoding=utf-8 tabstop=8 expandtab shiftwidth=4 softtabstop=4: # PEP 8
+# -*- coding: utf-8; -*-
+# vim: set expandtab fileencoding=utf-8 shiftwidth=4 softtabstop=4 tabstop=8:
 
 print "Hello, World!"
