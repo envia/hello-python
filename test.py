e58a7454fe5b89dd9efc59edbd3cb629340118ee
Hwang Joonhyung <hwang@base.envia.pe.kr>
2017-04-04 16:47:33 +0900

    test tempfile

--- test.py
+++ test.py
@@ -18,5 +18,18 @@ class TestHello(unittest.TestCase):
         r = random.randint(0, 9)
         self.assertEqual(r, 8)
 
+    def test_tempfile(self):
+        import filecmp
+        import shutil
+        import tempfile
+        s = tempfile.mkdtemp()
+        t = tempfile.mkdtemp()
+        with open(s + "/hello", "w") as f:
+            f.write("hello\n")
+        shutil.copy(s + "/hello", t + "/hello")
+        self.assertTrue(filecmp.cmp(s + "/hello", t + "/hello"))
+        shutil.rmtree(s)
+        shutil.rmtree(t)
+
 if __name__ == '__main__':
     unittest.main()
